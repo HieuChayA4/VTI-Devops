cc907c221ec75f276b0096102b1c5e0e168e540e
Hieu Quang <hieuchaya@gmail.com>
2023-08-24 17:33:19 +0700

    add keypair
    add keypair

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -5,5 +5,11 @@ resource "aws_instance" "this" {
   associate_public_ip_address = var.associate_public_ip_address
   subnet_id                   = "subnet-0987e4da014f80b75"
   security_groups             = ["sg-0b85f388c1731fb0e"]
+  key_name                    = var.key_pair
   tags                        = var.default_tags
+}
+
+resource "aws_key_pair" "key_pair" {
+  key_name   = var.key_pair
+  public_key = var.public_key
 }
