8d598ff9d3bee3534164dbc35e5f61352d9fc8cf
Hieu Quang <hieuchaya@gmail.com>
2023-09-05 16:44:47 +0700

    Create vpc, subnet, sg with tf
    Create vpc, subnet, sg with tf

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -6,4 +6,9 @@ terraform {
       name = "vti-demo-terraform"
     }
   }
+  # backend "s3" {
+  #   bucket = "vti2032-qhieu-tf-bucket"
+  #   key    = "s3://vti2032-qhieu-tf-bucket/tf/terraform.tfstate"
+  #   region = var.region
+  # }
 }
